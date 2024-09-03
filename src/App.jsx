78a904876e01bd05f991910ffb252aa8d91715c8
Aytaditya <adityaaryan531@gmail.com>
2024-09-04 01:52:17 +0530

    shadcn implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Badge } from './components/ui/badge'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-red-500'>
+        <h1 className='text-red-500 text-2xl pl-3'>hii</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Badge>hiiii</Badge>
+      <Button variant={"destructive"}>Button</Button>
     </>
   )
 }
